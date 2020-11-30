50cd1a539fde24431aa831b2a0cea019f095c9a4
donenrique <50317186+donenrique24@users.noreply.github.com>
2020-11-30 13:37:19 +0000

    Updating unitary tests

--- tests/testthat/examplesWebsite.R
+++ tests/testthat/examplesWebsite.R
@@ -16,15 +16,14 @@ library(BioSIM)
 locations <- BioSIM::twoLocationsInSouthernQuebec
 print(locations)
 
-variables <- c("TN","TX","P")
-summerMean <- getNormals("1981_2010", variables, locations$Name, locations$Latitude, locations$Longitude, locations$Elevation, c("June", "July", "August"))
+summerMean <- getNormals("1981_2010", locations$Name, locations$Latitude, locations$Longitude, locations$Elevation, c("June", "July", "August"))
 summerMean
 
 
-monthlyMeans <- getMonthlyNormals("1981_2010", variables, locations$Name, locations$Latitude, locations$Longitude, locations$Elevation)
+monthlyMeans <- getMonthlyNormals("1981_2010", locations$Name, locations$Latitude, locations$Longitude, locations$Elevation)
 monthlyMeans
 
-annualNormals <- getAnnualNormals("1981_2010", variables, locations$Name, locations$Latitude, locations$Longitude, locations$Elevation)
+annualNormals <- getAnnualNormals("1981_2010", locations$Name, locations$Latitude, locations$Longitude, locations$Elevation)
 annualNormals
 
 getModelList()
@@ -32,6 +31,10 @@ getModelList()
 addParms <- c("LowerThreshold"=5)
 degreeDays <- getModelOutput(2017, 2021, locations$Name, locations$Latitude, locations$Longitude, locations$Elevation, "DegreeDay_Annual", F, rcp = "RCP85", climModel = "GCM4", additionalParms = addParms)
 degreeDays
+
+MPB_SLR <- getModelOutput(2017, 2021, locations$Name, locations$Latitude, locations$Longitude, locations$Elevation, "MPB_SLR", F, rcp = "RCP85", climModel = "GCM4")
+
+
 
 shutdownJava()
 
